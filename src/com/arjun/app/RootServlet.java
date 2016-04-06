bbd8055f0221a300672a2fd19aa5a65cef00e771
arjun <linkin.drew1@gmail.com>
2016-04-07 01:54:03 +0500

    Milestone 1 : PMF.java added

---
+++ src/com/arjun/app/RootServlet.java
@@ -0,0 +1,32 @@
+package com.arjun.app;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Arrays;
+
+import javax.jdo.PersistenceManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.*;
+
+import com.google.api.server.spi.auth.common.User;
+import com.google.appengine.api.datastore.Key;
+import com.google.appengine.api.datastore.KeyFactory;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+@SuppressWarnings("serial")
+public class RootServlet extends HttpServlet {
+	private String output = "";
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException, ServletException {
+		// get a request dispatcher and launch a jsp that will render our page
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/root.jsp");
+		rd.forward(req, resp);
+	}
+
+	public void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+
+	}
+
+}
---
+++ war/WEB-INF/root.jsp
@@ -0,0 +1,14 @@
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>welcome</title>
+</head>
+
+</style>
+
+<body>
+	<h1>This is Appointment's App</h1>
+</body>
+</html>
